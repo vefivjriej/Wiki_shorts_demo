05f6055501a63c142552725c933ef2469cd12493
zzz <zaharzinin@gmail.com>
2025-04-03 20:13:19 +0300

    еще добавил

--- src/main/kotlin/Controller/GetAnArticle.kt
+++ src/main/kotlin/Controller/GetAnArticle.kt
@@ -1,10 +1,15 @@
 package Controller
 
+import Service.WikipediaDeepseekService
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("api/articles")
-class GetAnArticle {
-    
-}
+class GetAnArticle(
+    val service: WikipediaDeepseekService
+) {
+    @PostMapping("/summerized")
